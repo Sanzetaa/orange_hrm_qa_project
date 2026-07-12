0ac569cf7b0e8f68c9b98a1429b79af42ff61abc
Sanjita Adhikari <sanzeta23@gmail.com>
2026-07-12 10:17:18 +0545

    feat(admin): add search user functionality with tests

--- pages/admin_page.py
+++ pages/admin_page.py
@@ -1,10 +1,41 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from pages.base_page import BasePage
 
 class AdminPage(BasePage):
     ADMIN_HEADER = (By.XPATH, "//h6[text()='Admin']")
+    USERNAME_INPUT = (By.XPATH,  "//label[text()='Username']/ancestor::div[contains(@class,'oxd-input-group')]//input")
+    USER_ROLE_DROPDOWN = (By.XPATH, "//label[text()='User Role']/""ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]")
+    EMPLOYEE_NAME_INPUT = (By.XPATH, "//label[text()='Employee Name']/ancestor::div[contains(@class,'oxd-input-group')]//input")
+
+    STATUS_DROPDOWN = (By.XPATH, "//label[text()='Status']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]",)
+    SEARCH_BUTTON = (By.XPATH, "//button[@type='submit']")
+    RESET_BUTTON = (By.XPATH, "//button[@type='reset']")
+    RESULT_TABLE_ROWS = (By.XPATH, "//div[@class='oxd-table-body']/div[contains(@class,'oxd-table-card')]",)
+    NO_RECORDS_TEXT = (By.XPATH, "//span[text()='No Records Found']")
+
+    FIRST_ROW_USERNAME_CELL = (By.XPATH, "(//div[@class='oxd-table-body']/div[contains(@class,'oxd-table-card')])[1]""//div[contains(@class,'oxd-table-cell')][2]")
+
 
     def admin_page_is_displayed(self):
         return self.find_visible(self.ADMIN_HEADER)
+    
+    def search_user(self, username, timeout=10):
+        self.type(self.USERNAME_INPUT, username)
+        self.click(self.SEARCH_BUTTON, timeout)
+
+    def get_first_result_username(self):
+        return self.get_text(self.FIRST_ROW_USERNAME_CELL)
+    
+    def search_username_out_of_list(self):
+        return self.get_text(self.NO_RECORDS_TEXT)
+    
+    def no_record_text_is_displayed(self):
+        return self.find_visible(self.NO_RECORDS_TEXT)
+
+    
+
     
     
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,6 +1,7 @@
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from pages.admin_page import AdminPage
+import time
 
 def test_open_admin_page(driver):
 
@@ -14,3 +15,26 @@ def test_open_admin_page(driver):
     admin = AdminPage(driver)
 
     assert admin.admin_page_is_displayed()
+
+def test_search_user_by_username(driver):
+    login = LoginPage(driver)
+    login.load()
+    login.valid_credential_login("Admin", "admin123")
+
+    dashboard = DashboardPage(driver)
+    dashboard.click_admin_menu()
+    
+    admin = AdminPage(driver)
+
+    assert admin.admin_page_is_displayed()
+
+    admin.search_user("Admin")
+    time.sleep(5)
+    assert admin.get_first_result_username() == "Admin"
+
+def test_random_username(driver):
+    test_open_admin_page(driver)
+    admin = AdminPage(driver)
+    admin.search_user("Sanjita")
+    time.sleep(5)
+    assert admin.no_record_text_is_displayed()
